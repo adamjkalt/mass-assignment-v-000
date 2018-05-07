abe776dcec373ff6ab88ec158efb2a259692f3a3
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-07 18:55:05 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,15 @@
 class Person
 
-attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
-:weight, :handed, :complexion, :t_shirt_size, 
+attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+:weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
+def initialize(:name, :birthday, :hair_color, :eye_color, :height,
+:weight, :handed, :complexion, :t_shirt_size,
+:wrist_size, :glove_size, :pant_length, :pant_width)
+    @user_name = user_name
+    @name = name
+    @location = location
+    @age = age
+  end
 end
